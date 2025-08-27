23370ba72e9c6a580f152bc517433b28e19c05e4
Mohammadabuhammad <abuhammad2001@gmail.com>
2025-08-28 01:29:29 +0300

    commit

--- 08 - Algorithms & Problem Solving Level 4/08 - Algorithms & Problem Solving Level 4.cpp
+++ 08 - Algorithms & Problem Solving Level 4/08 - Algorithms & Problem Solving Level 4.cpp
@@ -9,80 +9,29 @@ int ReadNumber(string message) {
 	return Number;
 }
 
-string GetDayName(int DayOfWeekOrder) {
-	string names[] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
-	if (DayOfWeekOrder < 0 || DayOfWeekOrder > 6)
-		return "InvalidDay";
-	return names[DayOfWeekOrder];
-}
-
-string GetMonthName(int MonthOrder) {
-	string names[] = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-					   "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
-	if (MonthOrder < 1 || MonthOrder > 12)
-		return "InvalidMonth";
-	return names[MonthOrder - 1];
-}
+struct sDate
+{
+	int Year;
+	int Month;
+	int Day;
+};
 
 bool IsLeapYear(int year) {
 	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
 int NumberOfDaysInMonth(int month, int year) {
+	if (month < 1 || month > 12)
+		return 0;
+
 	int arrDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
 	if (month == 2)
 		return IsLeapYear(year) ? 29 : 28;
 	return arrDays[month - 1];
 }
 
-int GetDayIndex(int year, int month, int day) {
-	int a = (14 - month) / 12;
-	int y = year - a;
-	int m = month + 12 * a - 2;
-	return (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
-}
-//
-void PrintDayNames() {
-	for (int i = 0; i < 7; i++)
-		cout << setw(4) << GetDayName(i);
-	cout << endl;
-}
-
-void PrintDayNumber(int month, int year) {
-	int startDay = GetDayIndex(year, month, 1);
-	int days = NumberOfDaysInMonth(month, year);
-
-	for (int i = 0; i < startDay; i++)
-		cout << "    ";
-
-	for (int day = 1; day <= days; day++) {
-		cout << setw(4) << day;
-
-		if ((startDay + day) % 7 == 0)
-			cout << endl;
-	}
-	cout << endl;
-}
-
-void PrintMonthCalendar(int year, int month) {
-	cout << "\n__________" << GetMonthName(month) << "__________" << endl;
-	PrintDayNames();
-	PrintDayNumber(month, year);
-	cout << "_____________________________" << endl;
-}
-
-void PrintYearCalendar(int year) {
-	cout << "______________________________" << endl;
-	cout << "Calendar - " << year << endl;
-	cout << "______________________________" << endl;
-
-	for (int i = 1; i <= 12; i++) {
-		PrintMonthCalendar(year, i);
-		cout << endl;
-	}
-}
-
-int PrintTotalDaysFromTheBeginningOfYear(int year, int month , int day) {
+int PrintTotalDaysFromTheBeginningOfYear(int year, int month, int day) {
 
 	int TotalDays = 0;
 
@@ -95,15 +44,58 @@ int PrintTotalDaysFromTheBeginningOfYear(int year, int month , int day) {
 
 }
 
-int main() {
-	int year = ReadNumber("Please enter the year: ");
-	int month = ReadNumber("Please enter the month: ");
-	int day = ReadNumber("Please enter the day: ");
-	cout<<"\nNumber Of Days from the begining of the year is : "
-		<<PrintTotalDaysFromTheBeginningOfYear(year, month, day);
-	return 0;
+bool IsLastDayInMonth(int day, int month, int year) {
+
+	if (NumberOfDaysInMonth(month, year) == day)
+		return true;
+	return false;
+
 }
 
+bool IsLastMonthInYear(int month) {
+
+	if (month == 12)
+		return true;
+	return false;
+}
+
+sDate ReadFullDate() {
+	sDate Date;
+
+	Date.Day = ReadNumber("\nPlease enter a Day : ");
+	Date.Month = ReadNumber("\nPlease enter a Month : ");
+	Date.Year = ReadNumber("\nPlease enter a Year : ");
+
+	return Date;
+}
+
+int main() {
+	int day = ReadNumber("Please enter a Day : ");
+	int month = ReadNumber("Please enter a Month : ");
+	int year = ReadNumber("Please enter a Year : ");
+
+	if (IsLastDayInMonth(day, month, year)) {
+
+		cout << "\nYes , Day is last day in month. ";
+	}
+	else
+	{
+		cout << "\nNo , Day isn't last day in month. ";
+
+	}
+
+	if (IsLastMonthInYear(month)) {
+
+		cout << "\nYes, Month is last Month in year";
+	}
+	else
+	{
+		cout << "\nNo, Month isn't last Month in year";
+
+	}
+
+	return 0;
+}
 
 namespace problems {
 	class Problem1 {
@@ -404,7 +396,7 @@ namespace problems {
 			return 0;
 		}
 	};
-	class Problem7{
+	class Problem7 {
 		int ReadNumber(string message) {
 			int Number;
 			cout << message;
@@ -451,7 +443,7 @@ namespace problems {
 			return 0;
 		}
 	};
-	class Problem8ReviewTheCourse{
+	class Problem8ReviewTheCourse {
 		int ReadNumber(string message) {
 			int Number;
 			cout << message;
@@ -524,7 +516,7 @@ namespace problems {
 			return 0;
 		}
 	};
-	class Problem9{
+	class Problem9 {
 		int ReadNumber(string message) {
 			int Number;
 			cout << message;
@@ -608,6 +600,572 @@ namespace problems {
 		int main() {
 			int year = ReadNumber("Please enter the year: ");
 			PrintYearCalendar(year);
+			return 0;
+		}
+	};
+	class Problem10 {
+		int ReadNumber(string message) {
+			int Number;
+			cout << message;
+			cin >> Number;
+			return Number;
+		}
+
+		string GetDayName(int DayOfWeekOrder) {
+			string names[] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+			if (DayOfWeekOrder < 0 || DayOfWeekOrder > 6)
+				return "InvalidDay";
+			return names[DayOfWeekOrder];
+		}
+
+		string GetMonthName(int MonthOrder) {
+			string names[] = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+							   "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+			if (MonthOrder < 1 || MonthOrder > 12)
+				return "InvalidMonth";
+			return names[MonthOrder - 1];
+		}
+
+		bool IsLeapYear(int year) {
+			return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+		}
+
+		int NumberOfDaysInMonth(int month, int year) {
+			int arrDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+			if (month == 2)
+				return IsLeapYear(year) ? 29 : 28;
+			return arrDays[month - 1];
+		}
+
+		int GetDayIndex(int year, int month, int day) {
+			int a = (14 - month) / 12;
+			int y = year - a;
+			int m = month + 12 * a - 2;
+			return (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+		}
+		//
+		void PrintDayNames() {
+			for (int i = 0; i < 7; i++)
+				cout << setw(4) << GetDayName(i);
+			cout << endl;
+		}
+
+		void PrintDayNumber(int month, int year) {
+			int startDay = GetDayIndex(year, month, 1);
+			int days = NumberOfDaysInMonth(month, year);
+
+			for (int i = 0; i < startDay; i++)
+				cout << "    ";
+
+			for (int day = 1; day <= days; day++) {
+				cout << setw(4) << day;
+
+				if ((startDay + day) % 7 == 0)
+					cout << endl;
+			}
+			cout << endl;
+		}
+
+		void PrintMonthCalendar(int year, int month) {
+			cout << "\n__________" << GetMonthName(month) << "__________" << endl;
+			PrintDayNames();
+			PrintDayNumber(month, year);
+			cout << "_____________________________" << endl;
+		}
+
+		void PrintYearCalendar(int year) {
+			cout << "______________________________" << endl;
+			cout << "Calendar - " << year << endl;
+			cout << "______________________________" << endl;
+
+			for (int i = 1; i <= 12; i++) {
+				PrintMonthCalendar(year, i);
+				cout << endl;
+			}
+		}
+
+		int PrintTotalDaysFromTheBeginningOfYear(int year, int month, int day) {
+
+			int TotalDays = 0;
+
+			for (int i = 1; i <= month - 1; i++)
+			{
+				TotalDays += NumberOfDaysInMonth(i, year);
+			}
+			TotalDays += day;
+			return TotalDays;
+
+		}
+
+		int main() {
+			int year = ReadNumber("Please enter the year: ");
+			int month = ReadNumber("Please enter the month: ");
+			int day = ReadNumber("Please enter the day: ");
+			cout << "\nNumber Of Days from the begining of the year is : "
+				<< PrintTotalDaysFromTheBeginningOfYear(year, month, day);
+			return 0;
+		}
+	};
+	class Problem11Review {
+		int ReadNumber(string message) {
+			int Number;
+			cout << message;
+			cin >> Number;
+			return Number;
+		}
+
+		string GetDayName(int DayOfWeekOrder) {
+			string names[] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+			if (DayOfWeekOrder < 0 || DayOfWeekOrder > 6)
+				return "InvalidDay";
+			return names[DayOfWeekOrder];
+		}
+
+		string GetMonthName(int MonthOrder) {
+			string names[] = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+							   "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+			if (MonthOrder < 1 || MonthOrder > 12)
+				return "InvalidMonth";
+			return names[MonthOrder - 1];
+		}
+
+		bool IsLeapYear(int year) {
+			return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+		}
+
+		int NumberOfDaysInMonth(int month, int year) {
+			int arrDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+			if (month == 2)
+				return IsLeapYear(year) ? 29 : 28;
+			return arrDays[month - 1];
+		}
+
+		int GetDayIndex(int year, int month, int day) {
+			int a = (14 - month) / 12;
+			int y = year - a;
+			int m = month + 12 * a - 2;
+			return (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+		}
+
+		void PrintDayNames() {
+			for (int i = 0; i < 7; i++)
+				cout << setw(4) << GetDayName(i);
+			cout << endl;
+		}
+
+		void PrintDayNumber(int month, int year) {
+
+			int startDay = GetDayIndex(year, month, 1);
+			int days = NumberOfDaysInMonth(month, year);
+
+			for (int i = 0; i < startDay; i++)
+				cout << "    ";
+
+			for (int day = 1; day <= days; day++) {
+				cout << setw(4) << day;
+
+				if ((startDay + day) % 7 == 0)
+					cout << endl;
+			}
+			cout << endl;
+		}
+
+		void PrintMonthCalendar(int year, int month) {
+			cout << "\n__________" << GetMonthName(month) << "__________" << endl;
+			PrintDayNames();
+			PrintDayNumber(month, year);
+			cout << "_____________________________" << endl;
+		}
+
+		void PrintYearCalendar(int year) {
+			cout << "______________________________" << endl;
+			cout << "Calendar - " << year << endl;
+			cout << "______________________________" << endl;
+
+			for (int i = 1; i <= 12; i++) {
+				PrintMonthCalendar(year, i);
+				cout << endl;
+			}
+		}
+
+		int PrintTotalDaysFromTheBeginningOfYear(int year, int month, int day) {
+
+			int TotalDays = 0;
+
+			for (int i = 1; i <= month - 1; i++)
+			{
+				TotalDays += NumberOfDaysInMonth(i, year);
+			}
+			TotalDays += day;
+			return TotalDays;
+
+		}
+
+		void ConvertBackTotalDaysToDate(int totalOfDays, int year) {
+			int month = 1;
+			while (totalOfDays > NumberOfDaysInMonth(month, year)) {
+				totalOfDays -= NumberOfDaysInMonth(month, year);
+				month++;
+			}
+
+			int day = totalOfDays;
+
+			cout << "\nConverted Date: " << day << "/" << month << "/" << year
+				<< " (" << month << " ," << day << ", " << year << ")" << endl;
+		}
+
+
+		int main() {
+			int year = ReadNumber("Please enter the year: ");
+			int month = ReadNumber("Please enter the month: ");
+			int day = ReadNumber("Please enter the day: ");
+
+			int numberOfDays = PrintTotalDaysFromTheBeginningOfYear(year, month, day);
+			cout << "\nNumber Of Days from the beginning of the year is : "
+				<< numberOfDays << endl;
+
+			ConvertBackTotalDaysToDate(numberOfDays, year);
+
+			return 0;
+		}
+	};
+	class Problem12Review {
+		int ReadNumber(string message) {
+			int Number;
+			cout << message;
+			cin >> Number;
+			return Number;
+		}
+
+		struct sDate
+		{
+			int Year;
+			int Month;
+			int Day;
+		};
+
+		bool IsLeapYear(int year) {
+			return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+		}
+
+		int NumberOfDaysInMonth(int month, int year) {
+			if (month < 1 || month > 12)
+				return 0;
+
+			int arrDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+			if (month == 2)
+				return IsLeapYear(year) ? 29 : 28;
+			return arrDays[month - 1];
+		}
+
+		int PrintTotalDaysFromTheBeginningOfYear(int year, int month, int day) {
+
+			int TotalDays = 0;
+
+			for (int i = 1; i <= month - 1; i++)
+			{
+				TotalDays += NumberOfDaysInMonth(i, year);
+			}
+			TotalDays += day;
+			return TotalDays;
+
+		}
+
+		sDate AddingDaysToDate(int Days, sDate Date) {
+
+			int totalDays = Days + PrintTotalDaysFromTheBeginningOfYear(Date.Year, Date.Month, Date.Day);
+			int MonthDays = 0;
+			Date.Month = 1;
+			while (true)
+			{
+				MonthDays = NumberOfDaysInMonth(Date.Month, Date.Year);
+
+				if (totalDays > MonthDays) {
+					totalDays -= MonthDays;
+					Date.Month++;
+					if (Date.Month > 12) {
+						Date.Month = 1;
+						Date.Year++;
+					}
+				}
+				else
+				{
+					Date.Day = totalDays;
+					break;
+				}
+			}
+			return Date;
+		}
+
+		sDate ReadFullDate() {
+			sDate Date;
+
+			Date.Day = ReadNumber("\nPlease enter a Day : ");
+			Date.Month = ReadNumber("\nPlease enter a Month : ");
+			Date.Year = ReadNumber("\nPlease enter a Year : ");
+
+			return Date;
+		}
+
+		int main() {
+			sDate Date = ReadFullDate();
+			int Days = ReadNumber("How many days to add : ");
+
+			Date = AddingDaysToDate(Days, Date);
+
+			cout << "\nDate after adding [" << Days << "] days is: ";
+			cout << Date.Day << "/" << Date.Month << "/" << Date.Year << endl;
+
+			return 0;
+		}
+
+	};
+	class Problem13Review {
+
+		int ReadNumber(string message) {
+			int Number;
+			cout << message;
+			cin >> Number;
+			return Number;
+		}
+
+		struct sDate
+		{
+			int Year;
+			int Month;
+			int Day;
+		};
+
+		bool IsLeapYear(int year) {
+			return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+		}
+
+		int NumberOfDaysInMonth(int month, int year) {
+			if (month < 1 || month > 12)
+				return 0;
+
+			int arrDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+			if (month == 2)
+				return IsLeapYear(year) ? 29 : 28;
+			return arrDays[month - 1];
+		}
+
+		int PrintTotalDaysFromTheBeginningOfYear(int year, int month, int day) {
+
+			int TotalDays = 0;
+
+			for (int i = 1; i <= month - 1; i++)
+			{
+				TotalDays += NumberOfDaysInMonth(i, year);
+			}
+			TotalDays += day;
+			return TotalDays;
+
+		}
+
+		bool CompareTwoDates(sDate Date1, sDate Date2) {
+			return (Date1.Year > Date2.Year) ||
+				(Date1.Year == Date2.Year && Date1.Month > Date2.Month) ||
+				(Date1.Year == Date2.Year && Date1.Month == Date2.Month && Date1.Day > Date2.Day);
+		}
+
+
+		sDate ReadFullDate() {
+			sDate Date;
+
+			Date.Day = ReadNumber("\nPlease enter a Day : ");
+			Date.Month = ReadNumber("\nPlease enter a Month : ");
+			Date.Year = ReadNumber("\nPlease enter a Year : ");
+
+			return Date;
+		}
+
+		int main() {
+			sDate Date1 = ReadFullDate();
+			sDate Date2 = ReadFullDate();
+
+
+			if (!CompareTwoDates(Date1, Date2)) {
+				cout << "Date1 is Less then Date2";
+			}
+			else
+			{
+
+				cout << "Date1 is Bigger then Date2";
+			}
+
+
+			return 0;
+		}
+
+	};
+	class Problem14 {
+		int ReadNumber(string message) {
+			int Number;
+			cout << message;
+			cin >> Number;
+			return Number;
+		}
+
+		struct sDate
+		{
+			int Year;
+			int Month;
+			int Day;
+		};
+
+		bool IsLeapYear(int year) {
+			return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+		}
+
+		int NumberOfDaysInMonth(int month, int year) {
+			if (month < 1 || month > 12)
+				return 0;
+
+			int arrDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+			if (month == 2)
+				return IsLeapYear(year) ? 29 : 28;
+			return arrDays[month - 1];
+		}
+
+		int PrintTotalDaysFromTheBeginningOfYear(int year, int month, int day) {
+
+			int TotalDays = 0;
+
+			for (int i = 1; i <= month - 1; i++)
+			{
+				TotalDays += NumberOfDaysInMonth(i, year);
+			}
+			TotalDays += day;
+			return TotalDays;
+
+		}
+
+		bool IsDate1EqualDate2(sDate Date1, sDate Date2) {
+			return(Date1.Year == Date2.Year) ? ((Date1.Month == Date2.Month) ? (Date1.Day == Date2.Day) : false) : false;
+		}
+
+		sDate ReadFullDate() {
+			sDate Date;
+
+			Date.Day = ReadNumber("\nPlease enter a Day : ");
+			Date.Month = ReadNumber("\nPlease enter a Month : ");
+			Date.Year = ReadNumber("\nPlease enter a Year : ");
+
+			return Date;
+		}
+
+		int main() {
+			sDate Date1 = ReadFullDate();
+			sDate Date2 = ReadFullDate();
+
+			if (IsDate1EqualDate2(Date1, Date2)) {
+				cout << "Date1 is equal to Date2" << endl;
+			}
+			else {
+				cout << "Date1 is not equal to Date2" << endl;
+			}
+
+			return 0;
+		}
+
+	};
+	class Problem15{
+		int ReadNumber(string message) {
+			int Number;
+			cout << message;
+			cin >> Number;
+			return Number;
+		}
+
+		struct sDate
+		{
+			int Year;
+			int Month;
+			int Day;
+		};
+
+		bool IsLeapYear(int year) {
+			return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+		}
+
+		int NumberOfDaysInMonth(int month, int year) {
+			if (month < 1 || month > 12)
+				return 0;
+
+			int arrDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+			if (month == 2)
+				return IsLeapYear(year) ? 29 : 28;
+			return arrDays[month - 1];
+		}
+
+		int PrintTotalDaysFromTheBeginningOfYear(int year, int month, int day) {
+
+			int TotalDays = 0;
+
+			for (int i = 1; i <= month - 1; i++)
+			{
+				TotalDays += NumberOfDaysInMonth(i, year);
+			}
+			TotalDays += day;
+			return TotalDays;
+
+		}
+
+		bool IsLastDayInMonth(int day, int month, int year) {
+
+			if (NumberOfDaysInMonth(month, year) == day)
+				return true;
+			return false;
+
+		}
+
+		bool IsLastMonthInYear(int month) {
+
+			if (month == 12)
+				return true;
+			return false;
+		}
+
+		sDate ReadFullDate() {
+			sDate Date;
+
+			Date.Day = ReadNumber("\nPlease enter a Day : ");
+			Date.Month = ReadNumber("\nPlease enter a Month : ");
+			Date.Year = ReadNumber("\nPlease enter a Year : ");
+
+			return Date;
+		}
+
+		int main() {
+			int day = ReadNumber("Please enter a Day : ");
+			int month = ReadNumber("Please enter a Month : ");
+			int year = ReadNumber("Please enter a Year : ");
+
+			if (IsLastDayInMonth(day, month, year)) {
+
+				cout << "\nYes , Day is last day in month. ";
+			}
+			else
+			{
+				cout << "\nNo , Day isn't last day in month. ";
+
+			}
+
+			if (IsLastMonthInYear(month)) {
+
+				cout << "\nYes, Month is last Month in year";
+			}
+			else
+			{
+				cout << "\nNo, Month isn't last Month in year";
+
+			}
+
 			return 0;
 		}
 	};
